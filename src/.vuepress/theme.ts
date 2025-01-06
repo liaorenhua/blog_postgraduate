4beb6a5f11aa20c97d5d3c6f4393b88ddab868de
liao_rh <993929808@qq.com>
2025-01-06 17:50:51 +0800

    Revert "生活"
    This reverts commit 8b0f7fd8198278089573089bf25b8783418b0c62.

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,234 +1,234 @@
 import { hopeTheme } from "vuepress-theme-hope";
 
-import { enNavbarConfig, zhNavbarConfig } from "./navbar.js";
-import { enSidebarConfig, zhSidebarConfig } from "./sidebar/index.js";
+import navbar from "./navbar.js";
+import sidebar from "./sidebar.js";
 
-export default hopeTheme(
-  {
-    hostname: "https://mister-hope.com",
+export default hopeTheme({
+  hostname: "https://mister-hope.github.io",
 
-    author: {
-      name: "Mr.Hope",
-      url: "https://mister-hope.com",
-    },
+  author: {
+    name: "Mr.Leo",
+    url: "https://mister-hope.com",
+  },
 
-    favicon: "/favicon.ico",
+  iconAssets: "fontawesome-with-brands",
 
-    iconAssets: "//at.alicdn.com/t/font_2410206_vuzkjonf4s9.css",
+  logo: "https://theme-hope-assets.vuejs.press/logo.svg",
 
-    logo: "/logo.svg",
+  repo: "vuepress-theme-hope/vuepress-theme-hope",
 
-    repo: "Mister-Hope/Mister-Hope.github.io",
+  docsDir: "src",
 
-    repoDisplay: false,
+  // 导航栏
+  navbar,
 
-    docsDir: "src",
+  // 侧边栏
+  sidebar,
 
-    locales: {
-      "/": {
-        navbar: zhNavbarConfig,
-        sidebar: zhSidebarConfig,
+  // 页脚
+  footer: "默认页脚",
+  displayFooter: true,
 
-        footer:
-          '主题使用 <a href="https://theme-hope.vuejs.press/zh/">VuePress Theme Hope</a> | <a href="https://mister-hope.com/about/site.html" target="_blank">关于网站</a>',
-
-        copyright: "基于 MIT 协议，© 2019-至今 Mr.Hope",
-
-        blog: {
-          description: "理论物理硕士在读，前端开发者，开源项目贡献者",
-          intro: "/about/",
-          medias: {
-            GitHub: "https://github.com/Mister-Hope",
-            BiliBili: "https://space.bilibili.com/630395917",
-            QQ: "http://wpa.qq.com/msgrd?v=3&uin=1178522294&site=qq&menu=yes",
-            Qzone: "https://1178522294.qzone.qq.com/",
-            Gmail: "mailto:mister-hope@outlook.com",
-            Zhihu: "https://www.zhihu.com/people/mister-hope",
-            Steam: "https://steamcommunity.com/id/Mr-Hope/",
-            Weibo: "https://weibo.com/misterhope",
-            Gitee: "https://gitee.com/Mister-Hope",
-            Twitter: "https://twitter.com/Mister_Hope",
-            Telegram: "https://t.me/Mister_Hope",
-          },
-        },
-      },
-
-      "/en/": {
-        navbar: enNavbarConfig,
-        sidebar: enSidebarConfig,
-
-        footer:
-          'Theme by <a href="https://theme-hope.vuejs.press">vuepress-theme-hope</a>',
-
-        copyright: "MIT Licensed, © 2019-present Mr.Hope",
-
-        blog: {
-          description:
-            "VuePress project member, front-end developer, studying for a master's degree in theoretical physics",
-          intro: "/en/about/",
-          medias: {
-            Gmail: "mailto:mister-hope@outlook.com",
-            Steam: "https://steamcommunity.com/id/Mr-Hope/",
-            GitHub: "https://github.com/Mister-Hope",
-            Twitter: "https://twitter.com/Mister_Hope",
-            Telegram: "https://t.me/Mister_Hope",
-          },
-        },
-      },
-    },
-
-    displayFooter: true,
-    copyright: "Copyright © 2019-present Mr.Hope",
-
-    markdown: {
-      align: true,
-      codeTabs: true,
-      demo: true,
-      figure: true,
-      flowchart: true,
-      highlighter: {
-        type: "shiki",
-        lineNumbers: 10,
-        langAlias: {
-          conf: "ini",
-        },
-      },
-      imgLazyload: true,
-      imgMark: true,
-      imgSize: true,
-      footnote: true,
-      mermaid: true,
-      revealjs: true,
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-    },
-
-    plugins: {
-      blog: {
-        excerptLength: 0,
-      },
-
-      comment: {
-        provider: "Waline",
-        serverURL: "https://comment.mister-hope.com",
-      },
-
-      docsearch: {
-        appId: "92EXITUA7H",
-        apiKey: "33b44aab931cbbd821d1cf25727c68aa",
-        indexName: "mister-hope",
-      },
-
-      feed: {
-        atom: true,
-        json: true,
-        rss: true,
-      },
-
-      pwa: {
-        themeColor: "#5c92d1",
-        cacheHTML: false,
-        maxSize: 3072,
-        apple: {
-          icon: "/assets/icon/apple-touch-icon.png",
-          statusBarColor: "white",
-        },
-        msTile: {
-          image: "/assets/icon/ms-icon-144.png",
-          color: "#ffffff",
-        },
-        manifest: {
-          name: "Mr.Hope 的个人博客",
-          short_name: "Mr.Hope Blog",
-          description: "Mr.Hope 的个人博客",
-          theme_color: "#5c92d1",
-          icons: [
-            {
-              src: "/assets/icon/chrome-192.png",
-              sizes: "192x192",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-512.png",
-              sizes: "512x512",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-mask-192.png",
-              sizes: "192x192",
-              purpose: "maskable",
-              type: "image/png",
-            },
-            {
-              src: "/assets/icon/chrome-mask-512.png",
-              sizes: "512x512",
-              purpose: "maskable",
-              type: "image/png",
-            },
-          ],
-          shortcuts: [
-            {
-              name: "分类",
-              short_name: "分类",
-              icons: [
-                {
-                  src: "/assets/icon/category-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-              url: "/category/",
-              description: "文章分类分组",
-            },
-            {
-              name: "标签",
-              short_name: "标签",
-              icons: [
-                {
-                  src: "/assets/icon/tag-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-              url: "/tag/",
-              description: "文章标签分组",
-            },
-            {
-              name: "时间线",
-              short_name: "时间线",
-              icons: [
-                {
-                  src: "/assets/icon/timeline-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-              url: "/timeline/",
-              description: "时间线文章列表",
-            },
-            {
-              name: "个人介绍",
-              short_name: "个人介绍",
-              icons: [
-                {
-                  src: "/assets/icon/about-maskable.png",
-                  sizes: "192x192",
-                  purpose: "maskable",
-                  type: "image/png",
-                },
-              ],
-              url: "/about/",
-              description: "个人介绍",
-            },
-          ],
-        },
+  // 博客相关
+  blog: {
+    description: "金融IT领域资深开发者求学路",
+    intro: "/intro.html",
+    medias: {
+      Baidu: "https://example.com",
+      BiliBili: "https://example.com",
+      Bitbucket: "https://example.com",
+      Dingding: "https://example.com",
+      Discord: "https://example.com",
+      Dribbble: "https://example.com",
+      Email: "mailto:info@example.com",
+      Evernote: "https://example.com",
+      Facebook: "https://example.com",
+      Flipboard: "https://example.com",
+      Gitee: "https://example.com",
+      GitHub: "https://example.com",
+      Gitlab: "https://example.com",
+      Gmail: "mailto:info@example.com",
+      Instagram: "https://example.com",
+      Lark: "https://example.com",
+      Lines: "https://example.com",
+      Linkedin: "https://example.com",
+      Pinterest: "https://example.com",
+      Pocket: "https://example.com",
+      QQ: "https://example.com",
+      Qzone: "https://example.com",
+      Reddit: "https://example.com",
+      Rss: "https://example.com",
+      Steam: "https://example.com",
+      Twitter: "https://example.com",
+      Wechat: "https://example.com",
+      Weibo: "https://example.com",
+      Whatsapp: "https://example.com",
+      Youtube: "https://example.com",
+      Zhihu: "https://example.com",
+      VuePressThemeHope: {
+        icon: "https://theme-hope-assets.vuejs.press/logo.svg",
+        link: "https://theme-hope.vuejs.press",
       },
     },
   },
-  false,
-);
+
+  // 加密配置
+  encrypt: {
+    config: {
+      "/demo/encrypt.html": ["1234"],
+    },
+  },
+
+  // 多语言配置
+  metaLocales: {
+    editLink: "在 GitHub 上编辑此页",
+  },
+
+  // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
+  // hotReload: true,
+
+  // 此处开启了很多功能用于演示，你应仅保留用到的功能。
+  markdown: {
+    align: true,
+    attrs: true,
+    codeTabs: true,
+    component: true,
+    demo: true,
+    figure: true,
+    gfm: true,
+    imgLazyload: true,
+    imgSize: true,
+    include: true,
+    mark: true,
+    plantuml: true,
+    spoiler: true,
+    stylize: [
+      {
+        matcher: "Recommended",
+        replacer: ({ tag }) => {
+          if (tag === "em")
+            return {
+              tag: "Badge",
+              attrs: { type: "tip" },
+              content: "Recommended",
+            };
+        },
+      },
+    ],
+    sub: true,
+    sup: true,
+    tabs: true,
+    tasklist: true,
+    vPre: true,
+
+    // 取消注释它们如果你需要 TeX 支持
+    // markdownMath: {
+    //   // 启用前安装 katex
+    //   type: "katex",
+    //   // 或者安装 mathjax-full
+    //   type: "mathjax",
+    // },
+
+    // 如果你需要幻灯片，安装 @vuepress/plugin-revealjs 并取消下方注释
+    // revealjs: {
+    //   plugins: ["highlight", "math", "search", "notes", "zoom"],
+    // },
+
+    // 在启用之前安装 chart.js
+    // chartjs: true,
+
+    // insert component easily
+
+    // 在启用之前安装 echarts
+    // echarts: true,
+
+    // 在启用之前安装 flowchart.ts
+    // flowchart: true,
+
+    // 在启用之前安装 mermaid
+    // mermaid: true,
+
+    // playground: {
+    //   presets: ["ts", "vue"],
+    // },
+
+    // 在启用之前安装 @vue/repl
+    // vuePlayground: true,
+
+    // 在启用之前安装 sandpack-vue3
+    // sandpack: true,
+  },
+
+  // 在这里配置主题提供的插件
+  plugins: {
+    blog: true,
+
+    // 启用之前需安装 @waline/client
+    // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
+    // comment: {
+    //   provider: "Waline",
+    //   serverURL: "https://waline-comment.vuejs.press",
+    // },
+
+    components: {
+      components: ["Badge", "VPCard"],
+    },
+
+    // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cacheHTML: true,
+    //   cacheImage: true,
+    //   appendBase: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Demo",
+    //         short_name: "Demo",
+    //         url: "/demo/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
+  },
+});
